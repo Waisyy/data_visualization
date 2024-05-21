c8efd800add534ffb604cdcd5e1fc8068070ab4b
Waisyy <tabatchenkocemen2009@gmail.com>
2024-05-21 12:10:36 +0300

    1.0

---
+++ scatter_squares.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+
+x_values = list(range(1, 1001))
+y_values = [x**2 for x in x_values]
+
+plt.style.use('seaborn-v0_8')
+flg, ax = plt.subplots()
+ax.scatter(x_values, y_values, s=10)
+
+# Назначение заголовка диаграммы и меток осей.
+ax.set_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=24)
+ax.set_ylabel("Square of Value", fontsize=14)
+
+# Назначение диапазона для каждой оси.
+ax.axis([0, 1100, 0, 1100000])
+
+plt.show()
