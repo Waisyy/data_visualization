8fffe7304a9f5cfee593fee77f5f77872d499740
Waisyy <tabatchenkocemen2009@gmail.com>
2024-05-23 11:31:20 +0300

    RandomWalk

--- scatter_squares.py
+++ scatter_squares.py
@@ -5,7 +5,7 @@ y_values = [x**2 for x in x_values]
 
 plt.style.use('seaborn-v0_8')
 flg, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=10)
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=10)
 
 # Назначение заголовка диаграммы и меток осей.
 ax.set_title("Square Numbers", fontsize=24)
@@ -15,4 +15,4 @@ ax.set_ylabel("Square of Value", fontsize=14)
 # Назначение диапазона для каждой оси.
 ax.axis([0, 1100, 0, 1100000])
 
-plt.show()
+plt.savefig('squares_plot.png', bbox_inches='tight')
